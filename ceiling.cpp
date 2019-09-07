20a04ad941e2ebd0c95645e6048eac6b18bf913e
umadevi <umadevikarri4@gmail.com>
2019-09-07 17:06:28 +0530

    raptor basics class

---
+++ ceiling.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? c;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="enter 1st value";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="enter 2nd value";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   c =ceiling(a+b);
+
+   return 0;
+}
